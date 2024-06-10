1e11102c01a89ec49efa649ee345bfa88a3efbf7
KhalezAlex <klozevitz@yandex.ru>
2024-06-10 11:31:32 +0300

    Бот для обмена документами с бд

---
+++ dispatcher/src/main/java/org/klozevitz/controller/TelegramBot.java
@@ -0,0 +1,48 @@
+package org.klozevitz.controller;
+
+import lombok.Getter;
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@Log4j
+public class TelegramBot extends TelegramLongPollingBot {
+    @Getter
+    @Value("${bot.username}")
+    private String botUsername;
+    @Getter
+    @Value("${bot.token}")
+    private String botToken;
+    private final UpdateController updateController;
+
+    public TelegramBot(UpdateController updateController) {
+        this.updateController = updateController;
+    }
+
+    @PostConstruct
+    public void init() {
+        updateController.registerBot(this);
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        updateController.processUpdate(update);
+    }
+
+    public void sendAnswer(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e);
+            }
+        }
+    }
+}
---
+++ dispatcher/src/main/java/org/klozevitz/controller/UpdateController.java
@@ -0,0 +1,82 @@
+package org.klozevitz.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.klozevitz.sevice.interfaces.UpdateProducer;
+import org.klozevitz.util.MessageUtil;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import static org.klozevitz.model.RabbitQueue.*;
+
+@Log4j
+@Component
+@RequiredArgsConstructor
+public class UpdateController {
+    private TelegramBot telegramBot;
+    private final MessageUtil messageUtil;
+    private final UpdateProducer updateProducer;
+
+    public void registerBot(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    public void processUpdate(Update update) {
+        if(update == null) {
+            log.error("Received update is null");
+            return;
+        }
+
+        if (update.getMessage() != null) {
+            distributeMessageByType(update);
+        } else {
+            log.error("Unsupported received message type " + update);
+        }
+    }
+
+    private void distributeMessageByType(Update update) {
+        Message message = update.getMessage();
+        if (message.getText() != null) {
+            processTextMessage(update);
+        } else if (message.getDocument() != null) {
+            processDocMessage(update);
+        } else if (message.getPhoto() != null) {
+            processPhotoMessage(update);
+        } else {
+            setUnsupportedMessageTypeView(update);
+        }
+    }
+
+    private void setUnsupportedMessageTypeView(Update update) {
+        SendMessage sendMessage = messageUtil.generateSendMessageWithText(update,
+                "Неподдерживаемый тип сообщения!");
+        setView(sendMessage);
+    }
+
+    private void setFileIsReceivedView(Update update) {
+        SendMessage sendMessage = messageUtil.generateSendMessageWithText(update,
+                "Файл получен. Обрабатывается...");
+        setView(sendMessage);
+    }
+
+    private void setView(SendMessage sendMessage) {
+        telegramBot.sendAnswer(sendMessage);
+    }
+
+    private void processPhotoMessage(Update update) {
+        updateProducer.produce(PHOTO_MESSAGE_UPDATE, update);
+        setFileIsReceivedView(update);
+    }
+
+    private void processDocMessage(Update update) {
+        updateProducer.produce(DOC_MESSAGE_UPDATE, update);
+        setFileIsReceivedView(update);
+    }
+
+    private void processTextMessage(Update update) {
+        updateProducer.produce(TEXT_MESSAGE_UPDATE, update);
+    }
+}
+
